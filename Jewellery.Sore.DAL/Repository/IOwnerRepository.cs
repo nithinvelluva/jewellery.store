db794c76c50fef26a8371681e99b0689dbd09060
Nithin Narayanan <nithinvelluva@gmail.com>
2021-04-17 21:52:14 +0530

    removed unwanted files from DAL.

--- Jewellery.Sore.DAL/Repository/IOwnerRepository.cs
+++
@@ -1,9 +0,0 @@
-using Jewellery.Store.DAL.Entity;
-using System.Threading.Tasks;
-
-namespace Jewellery.Store.DAL.Repository
-{
-  public interface IOwnerRepository: IRepository<OwnerEntity>
-  {    
-  }
-}
